8bb34349d140db9a3a44d49d07490c1b395a652f
masao-yamakawa <masao-yamakawa@bm-sms.co.jp>
2017-05-10 15:27:43 +0900

    add method `getParams`.

--- src/main/kotlin/com/test/controller/page/MemoController.kt
+++ src/main/kotlin/com/test/controller/page/MemoController.kt
@@ -3,15 +3,12 @@ package com.test.controller.page
 import com.sun.corba.se.impl.protocol.giopmsgheaders.RequestMessage
 import org.springframework.ui.Model
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestMapping
 import sun.security.x509.OIDMap.addAttribute
 import java.util.ArrayList
 import com.test.model.Memo
 import com.test.service.MemoService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.*
 
 
 /**
@@ -59,6 +56,29 @@ class MemoController {
         model.addAttribute("items", items)
         return "memo"
     }
+
+    @RequestMapping("param/{memo:[a-zA-Z0-9]+}")
+    fun getParams(@PathVariable memo: String,
+                  @RequestParam(required = false, defaultValue = "Default Author") author: String,
+                  model: Model) : String {
+        /*
+        List<Memo> items = new ArrayList<>();
+        Memo item = new Memo();
+        item.setMemo(memo);
+        item.setAuthor(author);
+        items.add(item);
+
+        model.addAttribute("items", items);
+        return "memo";
+        */
+
+        var item = Memo()
+        item.memo = memo
+        item.author = author
+        model.addAttribute("items", listOf(item))
+        return "memo"
+    }
+
 
     @RequestMapping (value = "", method = arrayOf(RequestMethod.POST))
     fun post(@ModelAttribute item: Memo, model: Model): String {
--- src/test/kotlin/com/test/controller/page/MemoControllerSpek.kt
+++ src/test/kotlin/com/test/controller/page/MemoControllerSpek.kt
@@ -5,6 +5,7 @@ import com.test.service.MemoService
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.junit.Assert
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -49,4 +50,76 @@ class MemoControllerSpek : Spek ({
             assert(item.author == "author")
         }
     }
+
+    describe("/memo/param にGETでアクセスした時") {
+        on ("PathVariableの指定がなかった場合") {
+            val result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .get("/memo/param")).andReturn()
+            it ("HTTPステータスコードが404で返却される") {
+                Assert.assertEquals(404, result.response.status)
+            }
+        }
+        on ("PathVariableに英数字以外が指定の場合") {
+            val pathVariable = "value_123"
+            val result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .get("/memo/param/$pathVariable")).andReturn()
+            it ("HTTPステータスコードが404で返却される") {
+                Assert.assertEquals(404, result.response.status)
+            }
+        }
+        on ("PathVariableに英数字指定の場合") {
+            val pathVariable = "value123"
+            val result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .get("/memo/param/$pathVariable")).andReturn()
+            it ("HTTPステータスコードが200で返却される") {
+                Assert.assertEquals(200, result.response.status)
+            }
+            it ("pathVariable の内容が memo に格納される") {
+                val items = result.modelAndView.modelMap["items"]!! as List<*>
+                Assert.assertTrue(items.size == 1)
+
+                val item = items[0] as Memo
+                Assert.assertTrue(item.memo == pathVariable)
+            }
+        }
+        on ("RequestParamにauthor指定なしの場合") {
+            val memoValue = "memoValue"
+            val result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .get("/memo/param/$memoValue")).andReturn()
+            it ("HTTPステータスコードが200で返却される") {
+                Assert.assertEquals(200, result.response.status)
+            }
+            it ("authoe の内容に ’Default Author’ が格納される") {
+                val items = result.modelAndView.modelMap["items"]!! as List<*>
+                Assert.assertTrue(items.size == 1)
+
+                val item = items[0] as Memo
+                Assert.assertTrue(item.memo == memoValue)
+                Assert.assertTrue(item.author == "Default Author")
+            }
+        }
+        on ("RequestParamにauthor指定された場合") {
+            val memoValue = "memoValue"
+            val authorValue = "author1234"
+            val result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .get("/memo/param/$memoValue?author=$authorValue")).andReturn()
+            it ("HTTPステータスコードが200で返却される") {
+                Assert.assertEquals(200, result.response.status)
+            }
+            it ("author の内容に 指定された内容が格納される") {
+                val items = result.modelAndView.modelMap["items"]!! as List<*>
+                Assert.assertTrue(items.size == 1)
+
+                val item = items[0] as Memo
+                Assert.assertTrue(item.memo == "memoValue")
+                Assert.assertTrue(item.author == authorValue)
+            }
+        }
+    }
+
 })
